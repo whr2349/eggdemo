e143a6b65e7f4220c3126d6777bd78281a45cbfd
whr2349 <378237242@qq.com>
2020-06-22 08:54:42 +0800

    配置

--- app/controller/accont.js
+++ app/controller/accont.js
@@ -1,12 +1,12 @@
-'use strict';
+
 
 const Controller = require('egg').Controller;
 
 class AccontController extends Controller {
     async account() {
         const { ctx } = this;
-        let name = ctx.request.body.loginname;
-        let pwd = ctx.request.body.password;
+        const name = ctx.request.body.loginname;
+        const pwd = ctx.request.body.password;
         console.log(pwd)
         console.log(ctx.request.body)
     }
